69bf05cbaffd0faa1d9b6c434e5ddba4a2d3001c
YELYZAVETA ZINCHUK <107926338+mavka13@users.noreply.github.com>
2023-08-27 18:52:00 +0300

    add markup optimization

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,32 +1,33 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
+const markupGallery = galleryItems
+  .map((item) => {
+    return `<div class="gallery__item">
+  <a class="gallery__link" href="${item.original}">
+    <img
+      class="gallery__image"
+      src="${item.preview}"
+      data-source="${item.original}"
+      alt="${item.description}"
+    />
+  </a>
+</div>`;
+  })
+  .join("");
 
 
-console.log(galleryItems);
 
  const gallery = document.querySelector(".gallery");
+gallery.innerHTML = markupGallery;
 
 
- galleryItems.forEach((item) => {
-    const li = document.createElement("li")
-    li.className = "gallery__item";
-    const a = document.createElement("a")
-    a.className = "gallery__link";
-    a.href = item.original;
-    const img = document.createElement("img")
-    img.src = item.preview;
-    img.alt = item.description;
-    img.className = "gallery__image";
-    a.appendChild(img);
-    li.appendChild(a);
-    gallery.appendChild(li);
-    
-
- })
 
 const modalWindow = event =>{
     event.preventDefault();
+    if (!event.target.classList.contains("gallery__image")) {
+        return;
+      }
     const instance = basicLightbox.create(`<img src="${event.target.src}"width="800" height="600">`);
 
 instance.show()
@@ -34,3 +35,24 @@ instance.show()
 }
 
 gallery.addEventListener("click", modalWindow)
+
+
+
+
+
+//  galleryEl.forEach((item) => {
+//     const li = document.createElement("li")
+//     li.className = "gallery__item";
+//     const a = document.createElement("a")
+//     a.className = "gallery__link";
+//     a.href = item.original;
+//     const img = document.createElement("img")
+//     img.src = item.preview;
+//     img.alt = item.description;
+//     img.className = "gallery__image";
+//     a.appendChild(img);
+//     li.appendChild(a);
+//     gallery.appendChild(li);
+    
+
+//  })
--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,30 +1,55 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
-
-
-console.log(galleryItems);
+const markupGallery = galleryItems
+  .map((item) => {
+    return `<a class="gallery__item" href="${item.original}">
+  <img class="gallery__image" src="${item.preview}" alt="${item.description}" />
+</a>`;
+  })
+  .join("");
 
  const gallery = document.querySelector(".gallery");
-
-
- galleryItems.forEach((item) => {
-    const li = document.createElement("li")
-    li.className = "gallery__item";
-    const a = document.createElement("a")
-    a.className = "gallery__link";
-    a.href = item.original;
-    const img = document.createElement("img")
-    img.src = item.preview;
-    img.alt = item.description;
-    img.className = "gallery__image";
-    a.appendChild(img);
-    li.appendChild(a);
-    gallery.appendChild(li);
-    
-
- })
-
+ 
+ gallery.innerHTML = markupGallery;
 
 const lightbox = new SimpleLightbox('.gallery a', {captionsData: "alt", captionDelay: 250, captionType: "alt",  });
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// galleryItems.forEach((item) => {
+   //     const li = document.createElement("li")
+   //     li.className = "gallery__item";
+   //     const a = document.createElement("a")
+   //     a.className = "gallery__link";
+   //     a.href = item.original;
+   //     const img = document.createElement("img")
+   //     img.src = item.preview;
+   //     img.alt = item.description;
+   //     img.className = "gallery__image";
+   //     a.appendChild(img);
+   //     li.appendChild(a);
+   //     gallery.appendChild(li);
+       
+   
+   //  })
 
